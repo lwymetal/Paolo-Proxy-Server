9089d46efba42457831f5b08b03adaee8ca03458
Ubuntu <ubuntu@ip-172-31-12-58.us-west-1.compute.internal>
2018-07-14 05:10:30 +0000

    Minor tweaks

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
 const parser = require('body-parser');
-
 const app = express();
+const compression = require('compression');
 const PORT = 5000;
 
+app.use(compression()); 
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../static')));
+app.use(express.static(path.join(__dirname, '../static'), {maxAge: '7d'}));
 
 app.listen(PORT, err => {
   if (err) {
